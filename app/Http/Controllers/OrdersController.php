0b5514a013c17e2d7567d155f237319fd75262da
nopparat231 <23.noop@gmail.com>
2021-06-26 00:01:49 +0700

    Cf Shows By user->id

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Orders;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +16,44 @@ class OrdersController extends Controller
    *
    * @return \Illuminate\Http\Response
    */
+
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+
+
   public function index()
   {
-    return view("orders.MyOrder");
+    $menus =  DB::table('menus')
+      ->join('orders', 'orders.menu_id', '=', 'menus.id')
+      ->join('restaurants', 'restaurants.id', '=', 'menus.restaurant_id')
+      ->select(
+        'menus.id as menu_id',
+        'menus.user_id',
+        'menus.restaurant_id',
+        'menus.menu_name',
+        'menus.menu_img',
+        'menus.menu_detail',
+        'restaurant_name',
+        'orders.user_id',
+        'orders.menu_id',
+        'orders.orders_detail',
+        'orders.order_quantity',
+        'orders.orders_status',
+        'orders.created_at'
+      )
+      ->where('orders.user_id', '=', Auth::user()->id)
+      ->where('orders.orders_status', '!=', 2)
+      ->orderBy('menus.restaurant_id', 'DESC')
+      ->get();
+
+    $gmenus =  DB::table('menus')
+      ->select('restaurant_id', DB::raw('count(*) as total'))
+      ->groupBy('restaurant_id')
+      ->get();
+
+    return view('orders.MyOrder')->with(['menus' => $menus])->with(['gmenus' => $gmenus]);
   }
 
   /**
@@ -48,12 +85,7 @@ class OrdersController extends Controller
       ]
     );
 
-    return response()->json(
-      [
-        'success' => true,
-        'message' => 'Data inserted successfully'
-      ]
-    );
+    return redirect()->route('orders.MyOrder');
   }
 
   /**
@@ -65,9 +97,9 @@ class OrdersController extends Controller
   public function show($id)
   {
     $findn = DB::table('menus')
-            ->select('*')->where('restaurant_name', 'like', '%' . $request . '%')->limit(1)->get();
-        return $findn;
-    return view("orders.MyOrder");
+      ->select('*')->where('restaurant_name', 'like', '%' . $id . '%')->limit(1)->get();
+
+    return view("orders.MyOrder",$findn);
   }
 
   /**
--- resources/views/orders/MyOrder.blade.php
+++ resources/views/orders/MyOrder.blade.php
@@ -7,45 +7,52 @@
             <div class="row">
                 <div class="col-md-12">
 
-                    <div class="card mb-2" style="max-width: 240rem;">
-                        <div class="row g-0">
-                            <div class="col-md-2">
-                                <img src="https://cdn.pixabay.com/photo/2016/07/20/00/49/thailand-food-1529442_960_720.jpg" style="width: 100%;height: 100%;">
-                            </div>
-                            <div class="col-md-6">
-                                <div class="card-body">
-                                    <h5 class="card-title">กระเพราหมูสับ ไข่ดาว</h5>
-                                    <p class="card-text"><small class="text-muted">จำนวนคิวรอ <b id="orders_status"
-                                                data-id="1">1</b> คิว</small></p>
-                                    <input type="hidden" name="menu_id" class="menu_id"
-                                        value="1">
-                                        <input type="hidden" name="menu_name" class="menu_name" value="กระเพราหมูสับ ไข่ดาว">
-                                    <input type="hidden" class="qty-input" value="1">
-                                    <button type="button" class="add-to-cart-btn btn btn-success">ยืนยันการรับอาหารสำเร็จ</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <H1 style="text-align: center"><b>My Orders</b></H1>
+                    <hr>
 
-                    <div class="card mb-2" style="max-width: 240rem;">
-                        <div class="row g-0">
-                            <div class="col-md-2">
-                                <img src="https://cdn.pixabay.com/photo/2016/07/20/00/49/thailand-food-1529442_960_720.jpg" style="width: 100%;height: 100%;">
-                            </div>
-                            <div class="col-md-6">
-                                <div class="card-body">
-                                    <h5 class="card-title">กระเพราหมูสับ ไข่ดาว</h5>
-                                    <p class="card-text"><small class="text-muted">จำนวนคิวรอ <b id="orders_status"
-                                                data-id="1">1</b> คิว</small></p>
-                                    <input type="hidden" name="menu_id" class="menu_id"
-                                        value="1">
-                                        <input type="hidden" name="menu_name" class="menu_name" value="กระเพราหมูสับ ไข่ดาว">
-                                    <input type="hidden" class="qty-input" value="1">
-                                    <button type="button" class="add-to-cart-btn btn btn-success">ยืนยันการรับอาหารสำเร็จ</button>
+                    @for ($g = 0; $g < count($gmenus); $g++) <br>
+
+                        @foreach ($menus as $menu)
+                            @if ($menu->restaurant_id == $gmenus[$g]->restaurant_id)
+                                <h2 style="margin-bottom: 0px">{{ $menu->restaurant_name }}</h2>
+                            @break
+                        @endif
+                    @endforeach
+
+                    <hr style="margin-top: 0px">
+
+                    @foreach ($menus as $menu)
+
+                        @if ($menu->restaurant_id == $gmenus[$g]->restaurant_id)
+
+                            <div class="product_data">
+                                <div class="card mb-3" style="max-width: 540rem;">
+                                    <div class="row g-0">
+                                        <div class="col-md-2">
+                                            <img src="{{ $menu->menu_img }}" style="width: 100%;height: 100%;">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">{{ $menu->menu_name }}</h5>
+                                                <p class="card-text"><small class="text-muted">จำนวนคิวรอ <b
+                                                            id="orders_status" data-id="1">1</b> คิว</small></p>
+                                                <input type="hidden" name="menu_id" class="menu_id"
+                                                    value="{{ $menu->menu_id }}">
+                                                <input type="hidden" name="menu_name" class="menu_name"
+                                                    value="{{ $menu->menu_name }}">
+                                                <input type="hidden" class="qty-input" value="1">
+                                                <button type="button" disabled
+                                                    class="add-to-cart-btn btn btn-primary">รอทำอาหาร</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
+
                             </div>
-                        </div>
-                    </div>
+                        @endif
+                    @endforeach
+
+                    @endfor
 
                 </div>
             </div>
