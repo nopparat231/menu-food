2b2903bebe5eb9241a4069bdee72083664556242
nopparat231 <23.noop@gmail.com>
2021-08-07 22:15:41 +0700

    Update Order.js

--- public/js/Order.js
+++ public/js/Order.js
@@ -32,40 +32,40 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-    $(".done-order-btn").click(function (e) {
-        e.preventDefault();
+// $(document).ready(function () {
+//     $(".done-order-btn").click(function (e) {
+//         e.preventDefault();
 
-        $.ajaxSetup({
-            headers: {
-                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            },
-        });
+//         $.ajaxSetup({
+//             headers: {
+//                 "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+//             },
+//         });
 
-        var id = $(this).closest(".product_data").find(".orders_id").val();
-        var users_provider_id = $(this)
-            .closest(".product_data")
-            .find(".users_provider_id")
-            .val();
+//         var id = $(this).closest(".product_data").find(".orders_id").val();
+//         var users_provider_id = $(this)
+//             .closest(".product_data")
+//             .find(".users_provider_id")
+//             .val();
 
-        $.ajax({
-            url: "/update-orders/",
-            method: "POST",
-            data: {
-                _token: $("input[name=_token]").val(),
-                id: id,
-                orders_status: 3,
-                users_provider_id: users_provider_id,
-            },
-            success: function (response) {
-                alertify.set("notifier", "position", "top-right");
-                alertify.success("ทำอาหารเสร็จแล้ว");
-                //location.reload();
-                //cartload();
-            },
-        });
-    });
-});
+//         $.ajax({
+//             url: "/update-orders/",
+//             method: "POST",
+//             data: {
+//                 _token: $("input[name=_token]").val(),
+//                 id: id,
+//                 orders_status: 3,
+//                 users_provider_id: users_provider_id,
+//             },
+//             success: function (response) {
+//                 alertify.set("notifier", "position", "top-right");
+//                 alertify.success("ทำอาหารเสร็จแล้ว");
+//                 //location.reload();
+//                 //cartload();
+//             },
+//         });
+//     });
+// });
 
 $(document).ready(function () {
     $(".done-order-btn").click(function (e) {
